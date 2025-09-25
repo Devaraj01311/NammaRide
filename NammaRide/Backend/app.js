c978ddcfada30a5244971b3f20fa69456de7bbcc
Devaraj L <devarajldev01@gmail.com>
2025-09-25 15:21:47 +0530

    .added

--- NammaRide/Backend/app.js
+++ NammaRide/Backend/app.js
@@ -13,9 +13,10 @@ const rideRoutes = require('./routes/ride.route');
 connectToDb();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true,              
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- NammaRide/Backend/db/db.js
+++ NammaRide/Backend/db/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
-
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
-    ).then(() => {
-       console.log('Connected to DB');
-    }).catch(err => console.log(err));
-}
+    const uri = process.env.DB_CONNECT; // MongoDB URI from Render environment variables
+    if (!uri) {
+        console.error("Error: DB_CONNECT environment variable not set!");
+        process.exit(1); // stop server if no DB URI
+    }
 
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to DB'))
+    .catch(err => {
+        console.error('DB connection error:', err);
+        process.exit(1); // stop server if DB connection fails
+    });
+}
 
 module.exports = connectToDb;
