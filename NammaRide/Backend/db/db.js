781657ef825a771c75f7443fc2034ef1ba1d159f
Devaraj L <devarajldev01@gmail.com>
2025-09-25 15:23:42 +0530

    ..add

--- NammaRide/Backend/db/db.js
+++ NammaRide/Backend/db/db.js
@@ -1,21 +1,12 @@
 const mongoose = require('mongoose');
 
-function connectToDb() {
-    const uri = process.env.DB_CONNECT; // MongoDB URI from Render environment variables
-    if (!uri) {
-        console.error("Error: DB_CONNECT environment variable not set!");
-        process.exit(1); // stop server if no DB URI
-    }
 
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('Connected to DB'))
-    .catch(err => {
-        console.error('DB connection error:', err);
-        process.exit(1); // stop server if DB connection fails
-    });
+function connectToDb() {
+    mongoose.connect(process.env.DB_CONNECT
+    ).then(() => {
+       console.log('Connected to DB');
+    }).catch(err => console.log(err));
 }
+
 
 module.exports = connectToDb;
